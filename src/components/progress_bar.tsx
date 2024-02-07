f57091afe37c8432433250eee1f97510bd123f07
lawrence <lawcubegsm@gmail.com>
2024-02-07 22:00:03 +0000

    Add progress bar componnet

---
+++ src/components/progress_bar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProgressBar = () => {
+  return (
+    <div className="w-full bg-gray-200 rounded-full h-1.5 mb-4">
+      <div className="bg-blue-600 h-1.5 rounded-full dark:bg-blue-500 w-[45%]" ></div>
+    </div>
+  )
+}
+
+export default ProgressBar
