8cf7f6711dd5ff6ff2174cf8169fa3f819b2bdeb
lawrence <lawcubegsm@gmail.com>
2024-02-08 22:06:47 +0000

    Add cart product component

---
+++ src/components/cart/product.tsx
@@ -0,0 +1,70 @@
+import React, {useState} from 'react'
+import {products}  from "@/data/mock"
+import Link from 'next/link'
+import Image from 'next/image'
+import { Suspense } from 'react';
+import { CartProductSkeleton } from '../skeleton';
+import NumberInput from './numberInput';
+
+
+
+
+const CartProduct = () => {
+  const [inputValue, setInputValue] = useState(1)
+
+  return (
+    <Suspense  fallback={<CartProductSkeleton />}>
+      <div className="mt-8">
+        <div className="flow-root">
+          <ul role="list" className="-my-6 divide-y divide-gray-200">
+            {products.map((product) => (
+              <li key={product.id} className="flex py-6">
+                
+                <div className=" h-24 w-24 flex-shrink-0 flex content-center justify-center overflow-hidden rounded-md border border-gray-200">
+                  <Image
+                    src={product.imageSrc}
+                    alt={product.imageAlt}
+                    className="h-fit  w-fit self-center content-center"
+                    width={80}
+                    height={80}
+                  />
+                </div>
+
+                <div className="ml-4 flex flex-1 flex-col">
+                  <div>
+                    <div className="flex justify-between text-base text-gray-900">
+                      <h3>
+                        <a href={product.href}>{product.name}</a>
+                      </h3>
+                      <p className="ml-4">{product.price}</p>
+                    </div>
+                    <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                  </div>
+                  <div className="flex flex-1 items-end justify-between text-sm">
+                    <div className='flex gap-3'>
+                      <p className="text-gray-500">Qty : </p> 
+                      <NumberInput/>
+                    </div>
+                   
+                    
+                    <div className="flex">
+                      <button
+                        type="button"
+                        className="font-medium text-blue-context hover:text-blue-hover"
+                      >
+                        Remove
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </Suspense>
+
+  )
+}
+
+export default CartProduct
