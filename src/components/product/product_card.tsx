15ae6a39e5b8945cfebd0ded39d3f23c0a5e4b4a
lawrence <lawcubegsm@gmail.com>
2024-02-12 12:18:23 +0000

    Add product card

---
+++ src/components/product/product_card.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+const  ProductCard = () => {
+  return (
+    <div className='flex flex-col gap-2 border-[1px] h-fit w-fit p-2 ' >
+        <div>
+            <div className='relative flex justify-center content-center h-[15rem] w-[15rem] bg-gray-50'>
+                <Image src={'/img/alien2.png'} alt={''}
+                height = {450} 
+                width={450}
+                className='self-center '
+                />
+            </div>
+            <div className='flex flex-col'>
+                <div className='flex justify-between font-semibold '>
+                    <h2>Dell lat 3</h2>
+                    <p>$100</p>
+                </div>
+                <div className='flex flex-col gap-3 text-gray-500'>
+                    <div className='text-sm'>
+                        <p>Black </p>
+                    </div>
+
+                    <div className='flex justify-end w-full mb-2'>
+                        <Link href={'#'} className='rounded-full py-0.5 px-3  text-blue-context hover:text-blue-hover border hover:bg-gray-50  ring-1  '>View</Link>
+                    </div>
+                </div>
+           
+            </div>
+        </div>
+      
+
+    </div>
+  )
+}
+
+export default  ProductCard
---
+++ src/components/stars_rating/star.tsx
@@ -0,0 +1,42 @@
+'use client'
+import React, {useState} from 'react'
+
+interface Stars {
+  rate : number;
+  numberOfStars: number;
+  disableColor: boolean
+}
+
+
+const Star = ({rate = 4,  numberOfStars = 5  , disableColor=true} : Stars) => {
+  const [value, setValue] = useState(rate)
+
+  const handlePaint =(e : MouseEvent | any , value:  number) =>{
+        e.stopPropagation()
+        !disableColor && setValue(value)
+  }
+
+  const range=(value:number)=>{
+    let list = []
+    
+    for(let i = 0; i < value; i++ ){
+      list.push(i + 1)
+    }
+    return list
+  }
+ 
+  return (
+    <div className="flex items-center text-2xl">
+        {range(numberOfStars).map((el)=>(
+        <svg  key={el} className={`w-4 h-4 ${ value >= el ? `text-[#facc15]` : `text-gray-300 `} ms-1 `} 
+        onClick={(e) => handlePaint(e, el )} 
+        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+        </svg>
+        ))}
+    </div>
+    
+  )
+}
+
+export default Star
