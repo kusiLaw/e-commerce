a2513e07e3b665f51289f2bf7eccbb28c2131930
lawrence <lawcubegsm@gmail.com>
2024-02-15 13:57:27 +0000

    Add product review

--- src/components/product_reviews/customer_review.tsx
+++ src/components/product_reviews/customer_review.tsx
@@ -10,8 +10,8 @@ const  CustomerReview= ({name, rate, image, comment} :CustomersReview) => {
   return (
     <div className='w-full py-6 px-4 flex flex-col gap-4'>
         <div className='flex items-center gap-3'>
-            <div className='relative flex flex-col h-[3rem] w-[3rem] rounded-full p-2  '>
-              <Image src={image} alt={name} fill className='rounded-full outline  ring-black'/>
+            <div className='relative flex flex-col justify-center items-center content-center h-[3rem] w-[3rem] rounded-full  bg-blue-100  ring-3 ring-white '>
+              <Image src={image} alt={name} fill className='rounded-full '/>
             </div>
             <div className='flex flex-col gap-1'>
                 <h2 className='font-semibold'>{name}</h2>
