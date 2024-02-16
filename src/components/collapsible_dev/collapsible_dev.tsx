8f37b4dd4fc6744dc5f7c9ab6176efe6c5427316
lawrence <lawcubegsm@gmail.com>
2024-02-16 21:04:50 +0000

    Rename text_collapse to collapsible_dev

---
+++ src/components/collapsible_dev/collapsible_dev.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+// import { FaMinus, FaPlus } from 'react-icons/fa'
+import { FaPlus, FaMinus } from "react-icons/fa6";
+import { FiPlus } from "react-icons/fi";
+
+
+
+
+const TextCollapse = ({title= '', children, open = false}) => {
+  const [display, setDisplay] = useState(open)
+
+
+  return (
+    <div className='text-default_blue border-t border-b  py-2'>
+       <div className='flex flex-col gap-4'>
+          <div className='flex flex-wr/a justify-between items-center px- py-2'>
+           <h3 className='flex items-center '>
+             Features
+           </h3>
+           <div className='flex justify-end text-end font-normal' onClick={()=>{setDisplay(!display)}}>
+              {display ? <div className='text-gray-600'><FaMinus /> </div> :
+                  <div className='text-gray-600'> <FaPlus /> </div>
+              }
+           </div>
+          </div>
+
+          { display && <div className=' px-2 py-4 mb-2'>{children}</div>}
+
+       </div>
+    </div>
+  )
+}
+
+export default TextCollapse
