8a7d9a412aeaa5bf9c15e547d9b1d9c3505cf63a
lawrence <lawcubegsm@gmail.com>
2024-02-18 20:32:20 +0000

    Add related product component

---
+++ src/components/product/related_product.tsx
@@ -0,0 +1,98 @@
+'use client'
+
+import React, { useRef }   from 'react'
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import ProductCard from './product_card';
+import StyledComponentsRegistry from '@/lib/sty';
+
+
+const RelatedProduct = () => {
+    let sliderRef = useRef(null);
+
+    var settings = {
+      
+        dots: false,
+        infinite: true,
+        lazyLoad: 'ondemand',
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        pauseOnHover: true,
+        autoplaySpeed: 2000,
+        initialSlide: 0,
+        speed: 18000,
+      
+        // autoplay: true,
+        // speed: 2000,
+        // autoplaySpeed: 2000,
+        cssEase: "linear",
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        };
+  return (
+    <StyledComponentsRegistry>
+           <div className="slider-container w-[90%] mx-auto py-4 text-white ">
+    <Slider {...settings}>
+      {/* <div className='bg-blue-100 w-fit'> */}
+        <ProductCard/>
+      {/* </div> */}
+      <div>
+        <ProductCard/>
+      </div> 
+      <div>
+        <ProductCard/>
+      </div>
+      <div>
+        <ProductCard/>
+      </div>
+      <div>
+        <ProductCard/>
+      </div>
+      <div>
+        <ProductCard/>
+      </div>
+      <div>
+        <ProductCard/>
+      </div>
+      <div>
+        <ProductCard/>
+      </div>
+      <div>
+        <ProductCard/>
+      </div>
+    </Slider>
+  </div>
+    </StyledComponentsRegistry>
+ 
+ 
+  )
+}
+
+export default RelatedProduct
