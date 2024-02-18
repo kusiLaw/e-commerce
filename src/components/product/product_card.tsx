6d1db8c10e182b5e611bccc2bcde82acdc7e7157
lawrence <lawcubegsm@gmail.com>
2024-02-18 20:15:15 +0000

    Update product card component
    - add tag labels for sales, hot, new
    - add wish list button on component hover

--- src/components/product/product_card.tsx
+++ src/components/product/product_card.tsx
@@ -1,36 +1,42 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import WishListButton from './wishlist_button'
 
 
-const  ProductCard = () => {
+
+
+const  ProductCard = ({tag='saes'}) => {
   return (
-    <div className='flex flex-col gap-2 border-[1px] h-fit w-fit p-2 ' >
-        <div>
-            <div className='relative flex justify-center content-center h-[15rem] w-[15rem] bg-gray-50'>
-                <Image src={'/img/alien2.png'} alt={''}
-                height = {450} 
-                width={450}
+    <div  className='relative  group flex flex-col items-center  gap-2 border-[1px] h-fit w-[16rem] pt-1 mx-auto ' >
+            <Link href={'/'} className=' flex justify-center content-center h-[16rem] max-h-[15rem] w-[15rem]  max-w-[16rem] shadow-sm bg-gray-50 rounded-md mb- overflow-hidden'>
+                <Image src={'/img/hp_envy_1.jpg'} alt={''}
+                // fill
+                height = {180} 
+                width={180}
                 className='self-center '
                 />
-            </div>
-            <div className='flex flex-col'>
-                <div className='flex justify-between font-semibold '>
-                    <h2>Dell lat 3</h2>
-                    <p>$100</p>
+                {tag === 'sales' && <div className='absolute bg-blue-contrast1 text-white uppercase left-0 top-3 drop-shadow-md
+                 px-3 font-thin text-sm '>sales</div>}
+                { tag === 'new' && <div className='absolute bg-blue-context text-white uppercase left-0 top-3 drop-shadow-md px-3 font- text-sm '>new</div>}
+                { tag === 'hot' && <div className='absolute bg-red-900 text-white uppercase left-0 top-3 drop-shadow-md px-3 py-[1px] font- text-sm '>hot</div>}
+               <div className='hidden absolute right-1 top-1 z-10 group-hover:inline-block text-xl '>
+                  <WishListButton/>
+               </div>
+            </Link>
+            <div className='flex flex-col px-1.5 text-black w-full max-w-full '>
+               
+                <h2 className=' text-ellipsis text-nowrap overflow-hidden w-full'>Dell lat 3</h2>
+          
+                <div className='text-sm text-gray-500'>
+                    <p className=' text-ellipsis text-nowrap overflow-hidden w-full '>Black </p>
                 </div>
-                <div className='flex flex-col gap-3 text-gray-500'>
-                    <div className='text-sm'>
-                        <p>Black </p>
-                    </div>
-
-                    <div className='flex justify-end w-full mb-2'>
-                        <Link href={'#'} className='rounded-full py-0.5 px-3  text-blue-context hover:text-blue-hover border hover:bg-gray-50  ring-1  '>View</Link>
-                    </div>
+                <div className='flex justify-between w-full mb-2 mt-2'>
+                    <p className='text-ellipsis text-nowrap overflow-hidden w-full  font-bold '>$100</p>
                 </div>
-           
             </div>
-        </div>
+            
+        
       
 
     </div>
