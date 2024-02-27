16dddaf9ab0d9762f747c08ee1db566b25c6a1d9
lawrence <lawcubegsm@gmail.com>
2024-02-27 07:46:35 +0000

    Add progress bar

---
+++ src/components/form/progress_bar.tsx
@@ -0,0 +1,34 @@
+'use client'
+import React from 'react'
+
+
+interface ProgressBar {
+  percentage: number | string;
+  height: number;
+  bg: string;
+}
+
+const ProgressBar = ({percentage = 46, height = 0.75, bg='#facc15' }: ProgressBar) => {
+ 
+  return (
+    <div className={`w-full bg-gray-200 h-fit rounded-full`}>
+      <div className={`custom rounded-full h-3}
+       ${percentage}`}>
+  
+
+       </div>
+       <style jsx>{
+          `
+          .custom { 
+            width: ${percentage}%;
+            height: ${height}rem;
+            background: ${bg}
+            }
+          `
+        }          
+        </style>
+    </div>
+  )
+}
+
+export default ProgressBar
