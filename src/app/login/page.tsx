6d21f865c1de7104a46ae435262fa16e33d96860
lawrence <lawcubegsm@gmail.com>
2024-03-04 12:21:46 +0000

    Add login interface design

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import Login from "@/components/login";
+
+export default function SingIn() {
+    return (
+        <div className="flex justify-center items-center">
+         <Login/>
+        </div>
+       
+    );
+  }
+  
