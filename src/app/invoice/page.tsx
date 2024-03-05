10a1ed4cb83cb6f525aca98d1a5ab0e02e9445d9
lawrence <lawcubegsm@gmail.com>
2024-03-05 13:01:14 +0000

    Add initial invoice page design

---
+++ src/app/invoice/page.tsx
@@ -0,0 +1,10 @@
+import Invoice from '@/components/orders/invoice'
+import React from 'react'
+
+const OrderInvoice = () => {
+  return (
+    <div><Invoice/></div>
+  )
+}
+
+export default OrderInvoice
