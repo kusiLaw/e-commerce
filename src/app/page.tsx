fca5ffb26264bd233475731f0eef3f7776d36548
lawrence <lawcubegsm@gmail.com>
2024-03-18 15:39:54 +0000

    Update index page carousel design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,59 +19,66 @@ export default function Home() {
       <Nav/>
       <div className="mt-[4.5rem] w-full ">
         <main className="relative w-full px-2 md:px-3  ">
-          <section className={`relative    h-fit md:h-[calc(768px-5.5rem)]  `}>
+          <section className={`relative    h-[calc(568px-5.5rem)] md:h-[calc(768px-5.5rem)]  `}>
            <WelcomeCarousel>
-            <div className=" bg-[#05876b]/30 h-full flex flex-col justify-center items-center  ">
-              <div className="p-8 img_1 lg:ml-[8%] lg:mt-[4rem] h-full  w-full m-auto bg-[#eee]/90 lg:bg-transparent" >
-                <p className="font-bold te text-[2rem] md:text-[2.5rem] text-blue-context lg:text-inherit mt-4">Feel different ?</p>
-                <p className="font-bold text-[1.8rem] md:text-[2.5rem] text-blue-contrast lg:text-black/80 m">Original Men's Collections!</p>
-                <p className="mt-3 text-wite text-inherit ">Be on top of your dress and make others feel your presence</p>
-                <button
-                  type="submit"
-                  className={`${assistant.className} flex w-fit justify-center items-center gap-3 rounded-md bg-[#05876b]/70 font-semibold mt-8  px-4 py-3  font- leading-6 text-lg capitalize
-                  text-white shadow-sm  hover:bg-[#05876b] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 `}>
-                  Shop Now
-                  <IoIosArrowForward/>
-                </button>
-             </div>
+            <div className=" bg-[#05876b]/30 img_1  h-full w-full flex flex-col justify-center items-center  ">
+                <div className="my-[2rem] h-full  w-full m-auto    lg:bg-transparent" >
+                    <div className="w-[90%] h-[95%] m-auto bg-[#eee]/80 p-4 lg:py-10 lg:px-12 lg:bg-transparent ">
+                                <p className="font-bold te text-[1.8rem] md:text-[2.1rem] lg:text-[2.5rem] text-inherit mt-4">Feel different ?</p>
+                                <p className="font-bold text-[1.2rem] md:text-[2rem] lg:text-[2.3rem] text-black/80 mt-2">Original Men's Collections!</p>
+                                <p className="mt-3 text-wite text-inherit ">Be on top of your dress and make others feel your presence</p>
+                                <button
+                                  type="submit"
+                                  className={`${assistant.className} flex w-fit justify-center items-center gap-3 rounded-md bg-[#05876b]/70 font-semibold mt-8  px-4 py-3  font- leading-6 text-lg capitalize
+                                  text-white shadow-sm  hover:bg-[#05876b] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 `}>
+                                  Shop Now
+                                  <IoIosArrowForward/>
+                               </button>
+                    </div>
+               </div>
             </div>
 
             
-            <div className=" bg-[#418621]/30 h-full flex flex-col justify-center items-center  ">
-              <div className="p-8 img_kente lg:ml-[8%] lg:mt-[4rem] h-full  w-full m-auto bg-[#eee]/90 lg:bg-transparent " >
-                <p className="font-bold te text-[2rem] md:text-[2.5rem] text-blue-context lg:text-inherit mt-4 capitalize">Africa Cloth ?</p>
-                <p className="font-bold text-[1.8rem] md:text-[2.5rem] text-blue-contrast lg:text-black/80 c">Best suited for celebration!</p>
-                <p className="mt-3 text-wite text-inherit text-wrap">You can get both material and nice sew dress for men and women parties, weddings and more </p>
-                <button
-                  type="submit"
-                  className={`${assistant.className} flex w-fit justify-center items-center gap-3 rounded-md bg-[#418621]/70 font-semibold mt-8  px-4 py-3  font- leading-6 text-lg capitalize
-                  text-white shadow-sm shadow-[#418621]/50 hover:shadow-md hover:bg-[#418621] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 `}>
-                  Shop Now
-                  <IoIosArrowForward/>
-                </button>
-             </div>
+            <div className=" bg-[#418621]/30 img_kente h-full flex flex-col justify-center items-center  ">
+                <div className="my-[2rem] h-full  w-full m-auto    lg:bg-transparent" >
+                    <div className="w-[90%] h-[95%] m-auto bg-[#eee]/80 p-4 lg:py-10 lg:px-12 lg:bg-transparent ">
+                                <p className="font-bold te text-[1.8rem] md:text-[2.1rem] lg:text-[2.5rem] text-inherit mt-4">Africa Cloth ?</p>
+                                <p className="font-bold text-[1.2rem] md:text-[2rem] lg:text-[2.3rem] text-black/80 mt-2">Best suited for celebration!</p>
+                                <p className="mt-3 text-wite text-inherit w-[65%] text-wrap ">You can get both material and nice sew dress for men and women parties, weddings and more</p>
+                                <button
+                                   type="submit"
+                                    className={`${assistant.className} flex w-fit justify-center items-center gap-3 rounded-md bg-[#418621]/70 font-semibold mt-8  px-4 py-3  font- leading-6 text-lg capitalize
+                                    text-white shadow-sm shadow-[#418621]/50 hover:shadow-md hover:bg-[#418621] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 `}>
+                                    Shop Now
+                                   <IoIosArrowForward/>
+                               </button>
+                    </div>
+               </div>
             </div>
 
  
-            <div className=" bg-[#bbbec8]/30 h-full flex flex-col justify-center items-center  ">
-              <div className="p-8 img_direction lg:ml-[8%] lg:mt-[4rem] h-full   w-full m-auto bg-[#d3d1df]/90 lg:bg-transparent " >
-                <p className="font-bold te text-[2rem] md:text-[2.5rem] text-blue-context lg:text-inherit mt-4 ">Life made easy with Us</p>
-                <p className="font-bold text-[1.8rem] md:text-[2.5rem] text-blue-contrast lg:text-black/80 c">{ "Can't find your item here ? "}</p>
-                <p className="mt-3 text-wite text-inherit text-wrap">You can tell us the item you want and will delivery to you at you door. <br/> </p>
-                <button
-                  type="submit"
-                  className={`${assistant.className} flex w-fit justify-center items-center gap-3 rounded-md bg-blue-context font-semibold mt-8  px-4 py-3  font- leading-6 text-lg capitalize
-                  text-white shadow-sm shadow-blue-context/50 hover:shadow-md hover:bg-blue-hover focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 `}>
-                  Tell us what you want
-                  <IoIosArrowForward/>
-                </button>
-             </div>
+              <div className=" bg-[#bbbec8]/30 img_direction h-full flex flex-col justify-center items-center  ">
+                  <div className="my-[2rem] h-full  w-full m-auto    lg:bg-transparent" >
+                    <div className="w-[90%] h-[95%] m-auto bg-[#eee]/80 p-4 lg:py-10 lg:px-12 lg:bg-transparent ">
+                                <p className="font-bold te text-[1.6rem] md:text-[2.1rem] lg:text-[2.5rem] text-inherit mt-4">Life made easy with Us</p>
+                                <p className="font-bold text-[1.2rem] md:text-[2rem] lg:text-[2.3rem] text-black/80 mt-2">{ "Can't find your item here ? "}</p>
+                                <p className="mt-3 text-wite text-inherit w-[65%] text-wrap ">You can tell us the item you want and will delivery to you at you door.</p>
+                                <button
+                                     type="submit"
+                                      className={`${assistant.className} flex w-fit justify-center items-center gap-3 rounded-md bg-blue-context font-semibold mt-8  px-4 py-3  font- leading-6 text-lg capitalize
+                                      text-white shadow-sm shadow-blue-context/50 hover:shadow-md hover:bg-blue-hover focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 `}>
+                                      
+                                    Tell us what you want
+                                   <IoIosArrowForward/>
+                               </button>
+                    </div>
+                 </div>
             </div>
 
            </WelcomeCarousel> 
           </section>
           
-          <section className="w-full mt-20 mb-20">
+          <section className="w-full mt-14 md:mt-20 mb-20">
             <h2 className={` text-center flex flex-col gap-3 text-2xl font-medium capitalize after:h-[2px] after:w-12 after:self-center  after:bg-red-600`}>
               Categories
             </h2>
@@ -128,8 +135,8 @@ export default function Home() {
                       </button>
                 </div>
             </div>
-    
           </section>
+
           <section className="bg-back mt-10 mb-20 pt-10">
             <div className=" ">
               <h2 className={` text-center flex flex-col gap-3 text-2xl font-medium capitalize after:h-[2px] after:w-12 after:self-center  after:bg-red-600`}>
