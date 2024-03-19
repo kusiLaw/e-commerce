4191f5f6c633f17bb8ec4dd5ae172aa4e6598ffb
lawrence <lawcubegsm@gmail.com>
2024-03-19 22:07:41 +0000

    Fix pop-up component bug

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,26 +1,15 @@
 'use client'
-import React, { useRef, useReducer } from 'react'
-import { Fragment, useState } from 'react'
-import { HiOutlineShoppingBag } from "react-icons/hi2";
-import { FiShoppingCart } from "react-icons/fi";
-import { IoCartOutline } from "react-icons/io5";
-import { FaRegUser } from "react-icons/fa6";
-import { CiUser } from "react-icons/ci";
+import React, { useRef, useReducer,useState } from 'react'
 import Image from 'next/image';
 import Logo from '../logo/logo';
 import Cart from '../cart';
-import { LinkButton } from '../form/button';
 import Link from 'next/link';
 import ProfileDropDown from './profile';
 import { BiSupport } from "react-icons/bi";
-import { access } from 'fs';
 
 
 const Nav = () => {
-  // const [state, dispatch] = useReducer(reducer , {
-  //   openUser: false,
-
-  // })
+ 
   const [isOpen, setIsOpen] = useState(false)
   const [profile, setProfile] = useState(false)
   const [pageCover, setPageCover] = useState(false)
@@ -32,6 +21,11 @@ const Nav = () => {
     setPageCover(value)
   }
 
+  const closeAll=()=>{
+    setProfile(false)
+    setPageCover(false)
+    setDisplayCart(false)
+  }
 
   const handleCart = (e:any)=>{
     e.stopPropagation()
@@ -56,25 +50,26 @@ const Nav = () => {
               <Logo/>
             </div>
            
-            <div className='flex  gap-4 h-full text-black  w-fit'>
+            <div className='flex  gap-4 h-full w-fit'>
   
                 {/* <div className='border '> */}
                    
-                    <div className=' relative text-lg font- h-full  '>
+                    {/* <div className=' relative text-lg font- h-full  '>
                       <div className='hidden md:inline-flex h-full items-center justify-center '>
                         <p className='hover:text-blue-context '>Features</p>
                       </div>
-                    </div>
-                    <div className=' relative text-lg font- h-full hover:bg-gray-100 hover:border-b hover:border-blue-context '>
+                    </div> */}
+                    <div className=' relative text-lg  h-full  hover:border-b group hover:border-blue-context border-b  transition-all duration-500'>
                       <div className='hidden md:inline-flex h-full items-center justify-center '>
-                        <Link href={'/products'} className='hover:text-blue-context border-r-2 pr-4 capitalize' >products</Link>
+                        <Link href={'/products'} className='hover:text-blue-context border-r-2 pr-4 capitalize group-hover:text-blue-context' >products</Link>
                       </div>
-                    </div>
-                    <div className=' relative text-lg font- h-full  hover:border-b hover:border-blue-context border-b '>
-                      <div className='hidden md:inline-flex gap-1 h-full items-center justify-center '>
+                   </div>
+                
+              
+                    <div className=' relative text-lg h-full  hover:border-b group hover:border-blue-context border-b transition-all duration-500 '>
+                      <div className='hidden md:inline-flex gap-1 h-full items-center justify-center  group-hover:text-blue-context'>
                       <BiSupport/>
                         <p className='hover:text-blue-context border-r-2 pr-4'>Support</p>
-                        
                       </div>
                     </div>
                 {/* </div> */}
@@ -83,30 +78,21 @@ const Nav = () => {
 
                 <div className=' relative text-xl font-light h-full  '>
                     <button className='h-full flex items-center justify-center text-[1.4rem] gap-2 ' onClick={()=>{
-                    handleDropDowns(setProfile, !profile) }} >
-                      <Image alt={'profile'}src={'./user-alt-line.svg'} height={25} width={25} className=''/>
-                      
-                      {/* <span className='h-full flex items-center justify-center text-[1.2rem] w- hover:text-black '>
-                        <FaRegUser/></span> */}
-
+                  closeAll(); handleDropDowns(setProfile, !profile) }} >
+                          <Image alt={'profile'}src={'/user-alt-line.svg'} height={25} width={25} className=''/>
                     </button>
                   
-                  {profile && <ProfileDropDown isLogin={false} eventfn={handleDropDowns} args={setProfile}/>
-                  }
-
+                    {profile && <ProfileDropDown isLogin={false} eventfn={handleDropDowns} args={setProfile}/>}
                 </div>
 
                 {/* cart */}
                 <div className='relative w-fit  '>
-                  <button id='cart' className='flex bg-red-00 h-full items-center content-center text-[1.4rem] '
-                  onClick={()=>handleDropDowns(setDisplayCart, true)} >
-                   <Image alt={'profile'}src={'./shopping-cart.svg'} height={25} width={25} className=''
+                  <button id='cart' className='flex bg-red-00 h-full items-center content-center text-[1.4rem] text-bold '
+                  onClick={() => { closeAll(); setDisplayCart(true) }} >
+                   <Image alt={'profile'}src={'/shopping-cart.svg'} height={25} width={25} className=''
                       />
-
-                    {/* <span id='cart' className='h-full  flex items-center justify-center text-[1.5rem] w-8 hover:text-black'onClick={handleCart} ><IoCartOutline
-                   name='cart' onClick={handleCart}/></span>
-                    <span id='cart' className='absolute top-2 right-0 rounded-full text-sm  p-auto text-end hover:text-blue-context
-                    'onClick={handleCart}>0</span> */}
+                    <span id='cart' className='absolute text-center font-semibold top-1.5  w-8 -right-4 rounded-full text-sm  hover:text-blue-context text-red-500 border- overflow-clip text-ellipsis
+                    'onClick={() => { closeAll(); setDisplayCart(true) }}>0</span>
                   </button>
                 </div>
             </div>
@@ -121,7 +107,7 @@ const Nav = () => {
             joijpjpjpjpjpjpjpjopjpopoj
          </div>
         </div>}
-        {pageCover && <div  className='fixed left-0  right-0 h-full  bg-gray-600/30 shadow-xl z-20'>
+        {pageCover && <div  className='fixed left-0  right-0 h-full  bg-black/10 shadow-xl z-20'>
                 
                 </div>}
       </header>
