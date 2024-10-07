ecad66cd3252e74092743933a321fc2417465b48
lawrence <lawcubegsm@gmail.com>
2024-10-07 22:40:28 +0100

    Update server action error handle

--- src/lib/backend/server_actions.ts
+++ src/lib/backend/server_actions.ts
@@ -2,17 +2,26 @@
 
 import { revalidatePath } from "next/cache"
 import { baseUrl } from "./baseurls"
-import { notFound } from 'next/navigation'
+// import { notFound } from 'next/navigation'
+
+
+const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
+
+
 
 export async function getData(relativeUrl = '') {
-  const res = await fetch(baseUrl + relativeUrl);
-  
-    const data = await res.json()
-    console.log(data, 'utyty')
-    // return Response.json(data)
- 
-      revalidatePath('/')
-  
-    return data
+  try {
+      const res = await fetch(baseUrl + relativeUrl);
+console.log(baseUrl + relativeUrl)
+      const data = await res.json()
+    
+      // await sleep(1000)
+      // revalidatePath('/')
+      
+      return data
+  }catch{
+     return undefined
+  }
+
 }
 
