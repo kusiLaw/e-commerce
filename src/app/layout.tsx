cbddaf4047a6e7948854f9e0bdacb73dd6b8373f
lawrence <lawcubegsm@gmail.com>
2024-11-16 06:49:27 +0000

    Update layout with cart provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
 import { Inter, Open_Sans } from "next/font/google";
 import "./globals.css";
-import CustomProvider from "@/lib/redux/provider";
 import ToastSetup from "@/components/toast/toast";
-import StyledJsxRegistry from "@/lib/registry";
-import { Suspense } from "react";
+import CartProvider from "./provider";
 
 
 
@@ -28,13 +26,13 @@ export default function RootLayout({
       <body className={`${open_Sans.className} antialiased`}>
  
           <ToastSetup/>
-          {/* <StyledJsxRegistry> */}
-            <div className="flex flex-col items-center text-blue-950 ">
-            <div className="max-w-[1660px] w-full mx-auto ">
+        <CartProvider>
+            <div className="flex flex-col items-center text-blue-950 text-base">
+            <div className="max-w-[1920px] w-full mx-auto ">
                 {children}
               </div>
             </div>
-        {/* </StyledJsxRegistry> */}
+         </CartProvider>
 
       </body>
     </html>
