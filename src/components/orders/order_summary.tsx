733ee211f787713bf32b79919df0170f26066e3f
lawrence <lawcubegsm@gmail.com>
2024-11-28 07:11:42 +0000

    Update orders values

--- src/components/orders/order_summary.tsx
+++ src/components/orders/order_summary.tsx
@@ -1,16 +1,31 @@
 'use client'
 
-import React, { useRef } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+import { TotalCartQuantity, getLocaltCartItems, TotalCartPrice } from '@/helper'
+import { Frank_Ruhl_Libre } from 'next/font/google'
+import { updateShopingCartContext } from '@/app/provider'
 
- const OrderSummary = ({orders, shipping=0, tax=0}:{orders : Array<any> , shipping :any, tax : any}) => {
-  const ordertotal = orders.reduce((acc, order) => (acc + parseFloat(order.price ) ),0)
+const OrderSummary = ({ }: { orders?: Array<any>, shipping?: any, tax?: any }) => {
+      const { updateShopingCart } = useContext(updateShopingCartContext);
+    
+     
+      const [cart, setCart] = useState<any>( [])
+    let totalCartPrice = TotalCartPrice(cart) || 0
+     
+      useEffect(() => {
+        setCart(getLocaltCartItems())
+      }, [updateShopingCart])
+    
+    const tax = Number( totalCartPrice) > 0 ? totalCartPrice * 0.10 : 0
+    const shipping = 0
+  const ordertotal = Number( totalCartPrice)  + shipping + tax
   return (
     <div>
     <h2 className='text-lg font-medium'>Order Summary</h2>
         <div className='divide-y divide-gray-200'>
             <div className='flex justify-between py-3 '>
                 <p>Subtotal</p>
-                <p>$ {ordertotal}</p>
+                {/* <p>$ {ordertotal}</p> */}
             </div>
             <div className='flex justify-between py-3 '>
                 <p>Shipping</p>
@@ -18,11 +33,11 @@ import React, { useRef } from 'react'
             </div>
             <div className='flex justify-between py-3 '>
                 <p>Tax</p>
-                <p>${tax}</p>
+                <p>${tax.toFixed(2)}</p>
             </div>
             <div className='flex justify-between py-3  font-medium bg-gray-100'>
-                <p>Total Paid</p>
-                <p className='text-lg'>${parseFloat(shipping) + parseFloat(tax) + parseFloat(ordertotal) }</p>
+                <p>{'Order Total'}</p>
+                <p className='text-lg'>$ {ordertotal.toFixed(2) }</p>
             </div>
             
         </div>
