a3436f0ad4b3968f1e52095b3413a5796e44ffda
lawrence <lawcubegsm@gmail.com>
2024-12-07 07:22:07 +0000

    Update component using context

--- src/app/products/(common)/checkout/page.tsx
+++ src/app/products/(common)/checkout/page.tsx
@@ -1,15 +1,18 @@
 'use client'
-import { useState } from 'react'
+import { useState, useEffect, useContext  } from 'react'
 import OrderSummary from '@/components/orders/order_summary'
 import Image from 'next/image'
-import { products} from '@/data/mock'
-import CartProduct from '@/components/cart/product'
 import Input from '@/components/form/input'
 import { assistant } from '@/components/font'
 import CheckBox from '@/components/form/checkbox'
-import Link from 'next/link'
+import dynamic from 'next/dynamic'
 import Submit from '../../../../components/form/submit'
 
+
+
+const CartProduct = dynamic(() => import('@/components/cart/product'), {
+  ssr: false,
+})
 
 const Checkout = (
   {
@@ -22,23 +25,11 @@ const Checkout = (
   }
 ) => {
 
+    
+
+
   async function handleCheckout() {
-    // let u =   setTimeout(() => 'hghgfhfg',40000)
-    // console.log(formData.get('city'), 'hjhjhhjhjjhjhjhjjhj===', u)
-      
-    // check if the person is authentication
-    //if authenticated get data from server
-    //if not get data from local storage
-    // make transactions
   };
-
-
-// async function addToCart(formData) {
-//     'use server'
-//     const productId = formData.get('productId')
-//     await updateCart(productId)
-//   }
-
 
     //********* ENABLE THIS CODE IF YOU WANT HANDLE PAYMENT BY YOURSELF,  *******************/
 
@@ -55,7 +46,7 @@ const Checkout = (
       let card = event.target.value
       card = card.replace(/\s+/g, '')
       card = card.match(/.{1,4}/g) || [];
-      console.log(card.join(''))
+
       setFn(card.join('  '))
   }
 
@@ -63,12 +54,12 @@ const Checkout = (
 
 
 
-  let pro = products.slice(1,4) // fetch cart data either from server or localstorage
+ // fetch cart data either from server or localstorage
 
   return (
     <div className={`${assistant.className} bg-white w-full  flex justify-center mt-28 mb-20 `}>
       <div className=' w-[90%] flex flex-col-reverse md:flex-row gap-4 justify-center border rounded-md shadow- border-t-4 border-t-blue-context'>
-        <div className='w-full  md:w-[50%]   p-10 md:pr-12'>
+        <div className='w-full  md:w-[50%]  p-4  py-10 md:pr-12'>
           <form action={handleCheckout}>
             <div>
               <p className='capitalize mb-4 font-medium'>shipping address</p>
@@ -82,7 +73,7 @@ const Checkout = (
               <div className='mb-4'>
                   <Input label='apartment / suit' name='apartment' type='text' placeholder='Email' required/>
               </div>
-              <div className='mb-4 flex justify-between '>
+              <div className='mb-4 flex flex-col gap-4 lg:flex-row justify-between '>
                   <Input label='City' type='text'  name='city' placeholder='City'/>
                   <Input label='State / Province' type='text' name='state' placeholder='State Province' required/>
 
@@ -168,7 +159,7 @@ const Checkout = (
               </div>
            </div>
          
-      <div className={'mt-10'}>
+           <div className={'mt-10'}>
                 <Submit text='buy now' pending_text='making payment...'/>
               </div>
             
@@ -177,12 +168,12 @@ const Checkout = (
 
           </form>
         </div>
-        <div className='w-full md:w-[50%] divide-y bg-gray-50 p-10 pl-12'>
-           <div className='mb-12'>
-               <CartProduct />
+        <div className='w-full md:w-[50%] divide-y bg-gray-100 py-10 px-4 lg:p-10 '>
+           <div className='mb-12 flex flex-col gap-4 justify-center items-center w-full'>
+               <CartProduct disabledNumberInput={false} />
            </div>
           <div className='pt-10                          '>
-             <OrderSummary orders={pro} tax={57} shipping={0}/>
+             <OrderSummary />
           </div>
         </div>
       </div>
