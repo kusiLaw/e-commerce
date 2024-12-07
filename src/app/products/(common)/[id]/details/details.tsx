58ce2de919441344e6bd2e7fff0664c32cd0bebf
lawrence <lawcubegsm@gmail.com>
2024-12-07 07:21:20 +0000

    fix hydration error

--- src/app/products/(common)/[id]/details/details.tsx
+++ src/app/products/(common)/[id]/details/details.tsx
@@ -3,21 +3,21 @@ import ProductDetails from '@/components/product/product_detail'
 import { getData } from '@/lib/backend/server_actions'
 
 
-
-
-export default async function Detail({productId}:{productId: string | number}) {
+export default async function Detail({ productId }: { productId: string | number }) {
     
- const product = await getData(`v1/products/${productId}/`)
+ const products = await getData()
   
+  const product = products?.results.filter((item: any) => item.id === Number(productId));
+
     
-    if (!product) {
+  if (!product) {
     return (
        <div className='w-full h-[8rem] text-center'>server error</div>
      )
    }
        
   return (
-    <ProductDetails product={product} />
+    <ProductDetails product={product[0]} />
 
   )
 }
